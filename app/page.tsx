0e9feb98c1cdea354e5406a24fd60cb58340c966
Levon Aslanyan <aslanyan2012@mail.ru>
2024-07-16 01:31:05 +0400

    start

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <Button size="sm">Click me</Button>
+      
+    </>
+  );
+};
+
+export default Home;
