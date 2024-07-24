884527d00925f2ede9e99f808c37b795b397b820
Levon Aslanyan <aslanyan2012@mail.ru>
2024-07-25 00:39:23 +0400

    Auth started and finished part 1

---
+++ components/auth/loading.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+type Props = {};
+
+const Loading = (props: Props) => {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={120}
+        height={120}
+        className="animate-pulse duration-700"
+      />
+    </div>
+  );
+};
+
+export default Loading;
