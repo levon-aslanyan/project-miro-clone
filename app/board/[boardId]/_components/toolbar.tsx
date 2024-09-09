a0114bcec7169433f6d243fa0d7f202e8457b873
Levon Aslanyan <aslanyan2012@mail.ru>
2024-09-10 01:12:16 +0400

    logo, title info

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -18,7 +18,7 @@ const Toolbar = () => {
   );
 };
 
-Toolbar.Skeleton = function ToolbarSkeleton() {
+export const ToolbarSkeleton = () => {
   return (
     <div className="flex flex-col gap-y-4 absolute shadow-md rounded-md  top-[50%] -translate-y-[50%] left-2 bg-white w-[51px] h-[360px]" />
   );
